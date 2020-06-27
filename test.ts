93fc0a5b4a7f3487a49bff411adf512dd0aa561d
Kitson Kelly <me@kitsonkelly.com>
2020-06-27 13:52:07 +1000

    Update to Deno 1.1.2, std 0.59.0

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 
 const { test } = Deno;
-import { assertEquals } from "https://deno.land/std@v0.58.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@v0.59.0/testing/asserts.ts";
 import {
   lookup,
   contentType,
