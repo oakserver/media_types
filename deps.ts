06cb129b8ea72bb5edf17c83a94d025c5476989b
Kitson Kelly <me@kitsonkelly.com>
2020-07-04 13:20:22 +1000

    Update to Deno 1.1.3, std 0.60.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 
-export { extname } from "https://deno.land/std@0.59.0/path/mod.ts";
+export { extname } from "https://deno.land/std@0.60.0/path/mod.ts";
