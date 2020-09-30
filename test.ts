88d85407fa047f8b7c2b53f178c4e86a65b5aafd
Kitson Kelly <me@kitsonkelly.com>
2020-09-30 14:33:57 +1000

    Update to Deno 1.4.2, std 0.71.0

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 // Copyright 2020 the oak authors. All rights reserved. MIT license.
 
-import { assertEquals } from "https://deno.land/std@0.69.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.71.0/testing/asserts.ts";
 import {
   charset,
   contentType,
